920a6dff1be67b6927ed6d1e14224f8c1c990cde
Dave Naugler <davenaugler@gmail.com>
2023-07-21 19:06:12 -0600

    Removed comments and cleaned up the code

--- src/main/SetExerciseApplication.java
+++ src/main/SetExerciseApplication.java
@@ -6,15 +6,8 @@ import java.util.Set;
 public class SetExerciseApplication {
 	// --- Instantiate new HashSet --- //
 	static Set<String> pokerHands = new HashSet<>();
-	// Looking to get some understanding for why Trevor wanted to instantiate the new 
-	// HashSet outside of the main method. In my case, it's not needed. Probably because 
-	// I instantiate it inside my FileService.java file
 	
 	public static void main(String[] args) {
-		// --- Extract the data from the CSV --- //
-		// Original extractDataFromCSV() method
-//		Set<String> pokerHands = new HashSet<>();
-		
 		// Updated extractDataFromCSV() request
 		Set<String> pokerHands = extractDataFromCSV("PokerHands.csv"); // What should be in these parentheses? 
 		printToConsole(pokerHands);
@@ -35,13 +28,6 @@ public class SetExerciseApplication {
 		}
 		System.out.println("");
 	}
-	
-	// Original extractDataFromCSV() method
-//	public static Set<String> extractDataFromCSV(String fileName) {
-//		FileService_2 fileService = new FileService_2();
-//		fileService.readFile();
-//		return null;
-//	}
 
 	// Updated extractDataFromCSV() method
 	public static Set<String> extractDataFromCSV(String fileName) {
