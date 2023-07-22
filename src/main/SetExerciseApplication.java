b3c3a89565db3172c3738a4a466be846560fee05
Dave Naugler <davenaugler@gmail.com>
2023-07-21 19:12:50 -0600

    Final commit of clean up

--- src/main/SetExerciseApplication.java
+++ src/main/SetExerciseApplication.java
@@ -6,18 +6,18 @@ import java.util.Set;
 public class SetExerciseApplication {
 	// --- Instantiate new HashSet --- //
 	static Set<String> pokerHands = new HashSet<>();
-	
+
 	public static void main(String[] args) {
-		// Updated extractDataFromCSV() request
-		Set<String> pokerHands = extractDataFromCSV("PokerHands.csv"); // What should be in these parentheses? 
+		// --- Extract the data from the CSV --- //
+		Set<String> pokerHands = extractDataFromCSV("PokerHands.csv"); // What should be in these parentheses?
 		printToConsole(pokerHands);
-		
+
 		// --- Remove ACE HIGH and QUEEN HIGH --- //
 		removeCard(pokerHands, "ACE HIGH");
 		removeCard(pokerHands, "QUEEN HIGH");
 		printToConsole(pokerHands);
 
-		// --- Update PAIR to DEUCES --- // 
+		// --- Update PAIR to DEUCES --- //
 		updateCard(pokerHands, "PAIR", "DEUCES");
 		printToConsole(pokerHands);
 	}
@@ -29,8 +29,7 @@ public class SetExerciseApplication {
 		System.out.println("");
 	}
 
-	// Updated extractDataFromCSV() method
-	public static Set<String> extractDataFromCSV(String fileName) {
+	private static Set<String> extractDataFromCSV(String fileName) {
 		FileService fileService = new FileService();
 		return fileService.readFile(fileName);
 	}
